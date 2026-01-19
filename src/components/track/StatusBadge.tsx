aeca2f745d89bfad9f1e04fc5f7841acafdc3aab
khush <khushpreet.singh@autviz.in>
2026-01-19 17:33:20 +0530

    status badge fix

--- src/components/track/StatusBadge.tsx
+++ src/components/track/StatusBadge.tsx
@@ -130,7 +130,7 @@ const styles = StyleSheet.create({
     borderColor: Colors.primaryTeal,
     borderRadius: 20,
     paddingHorizontal: 12,
-    paddingVertical: 6,
+    paddingVertical: 2,
   },
   badge: {
     flexDirection: 'row',
